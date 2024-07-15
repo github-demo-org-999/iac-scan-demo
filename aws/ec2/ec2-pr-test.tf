d1d82e3b5f5c3e71eb61c8bf3d9e8c96e994a1b1
github-demo-org-999 <github-demo@abhishekm.in>
2024-07-15 15:48:28 +0530

    Update ec2-pr-test.tf

--- aws/ec2/ec2-pr-test.tf
+++ aws/ec2/ec2-pr-test.tf
@@ -61,7 +61,7 @@ resource "aws_security_group" "demo_sg" {
   }
 }
 
-resource "aws_instance" "demo_instance" {
+resource "aws_instance" "demo_instance_2" {
   ami           = "ami-0c55b159cbfafe1f0" # Update with your desired AMI ID
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.demo_subnet.id
